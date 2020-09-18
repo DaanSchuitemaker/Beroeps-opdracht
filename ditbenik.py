d45186288301e919470eafe340942f3602c2dcbc
DaanSchuitemaker <70897031+DaanSchuitemaker@users.noreply.github.com>
2020-09-18 14:44:36 +0200

    Dit ben ik opdracht (vragenlijst)

---
+++ ditbenik.py
@@ -0,0 +1,36 @@
+print('Hello You!, ik ben Daan')
+print('Wie ben jij?')
+naam = input()
+print('Hello ' + naam)
+print('Voor ik naar het ma ging heb ik een aantal maanden een HBO gedaan, welke?')
+print('A. HBO ICT bij HVA')
+print('B. HBO bedrijfseconomie HVA')
+print('C. HBO life science & chemistry Inholland Amsterdam')
+q1_ans = input('jouw antwoord:')
+if q1_ans == 'C' or q1_ans == 'c':
+	print('Correct')
+else:
+	print('Incorrect')
+
+print()
+print('Voor ik die HBO had gedaan zat ik op de middelbare school, welke school?')
+print('A. RSG Enkhuizen')
+print('B. Martinuscollege Grootebroek')
+print('C. Clusiuscollege Hoorn')
+q2_ans = input('jouw antwoord:')
+if q2_ans == 'A' or q2_ans == 'a':
+	print('Correct')
+else:
+	print('Incorrect')
+
+print()
+print('Welke route wil ik bij het ma volgen?')
+print('A. Game Artist')
+print('B. Game developer')
+print('C. Software developer')
+q3_ans = input('jouw antwoord:')
+if q3_ans == 'B' or q3_ans == 'b':
+	print('Correct')
+else:
+	print('Incorrect')
+
